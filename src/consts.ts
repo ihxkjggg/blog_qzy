48f1902db47f2b44c38aea2974900337730e157e
ihxkjggg <ihxkjggg@126.com>
2024-04-06 16:30:13 +0800

    20240406

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '强资源';
+export const SITE_DESCRIPTION = '欢迎来到我的软件分享博客!';
